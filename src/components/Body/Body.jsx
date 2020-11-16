72862a778726876c1159153b7f894c0bea2efea3
Nursultan Akhmetzhanov <nursmaster@gmail.com>
2020-11-16 13:13:03 +0200

    Completed task 2: - Added Body component - Set its top margin to 100px - Assigned width and height values to it - Placed right under the Header component

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Body.module.css';
+
+const Body = () => {
+    return (
+        <div className={styles.container} >
+
+        </div>
+    );
+};
+
+export default Body;
