444024751b1fe7f23bd5ed16ba9cfff8fab884a5
Nursultan Akhmetzhanov <nursmaster@gmail.com>
2020-11-16 13:35:53 +0200

    Completed task 3: - Added antdesign library - Put a button there - Created a function and a useState state for background color change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 
 import Header from './components/Header/Header';
 import Body from './components/Body/Body';
--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from './Body.module.css';
+import {Button} from "antd";
 
 const Body = () => {
-    return (
-        <div className={styles.container} >
 
+    const [isOn, setIsOn] = React.useState(false);
+
+    const setIsOnHandler = () => {
+        setIsOn(!isOn);
+    }
+
+    return (
+        <div className={`${styles.container} ${ isOn ? styles.lightBackground : styles.darkBackground}`} >
+            <Button type={ isOn ? 'ghost' : 'primary'} size={'large'} onClick={setIsOnHandler} >
+                { isOn ? `OFF` : `ON`}
+            </Button>
         </div>
     );
 };
